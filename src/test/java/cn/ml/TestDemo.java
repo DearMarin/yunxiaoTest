b796e3d534fbe0611739d73b4ad2c3977e667ccb
marin <1748896973@qq.com>
2022-08-08 09:44:40 +0800

    增加错误测试用例

--- src/test/java/cn/ml/TestDemo.java
+++ src/test/java/cn/ml/TestDemo.java
@@ -1,5 +1,6 @@
 package cn.ml;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class TestDemo {
@@ -15,10 +16,21 @@ public class TestDemo {
 			Demo demo = new Demo();
 			System.out.println(demo.reduce(1, 2));
 		}
+		//跳过测试
+		@Test
+		@Ignore
+		public void test() {
+			System.out.println("跳过测试");
+		}
 		//乘法
 		@Test
 		public void test3() {
 			Demo demo = new Demo();
 			System.out.println(demo.ride(1, 2));
-		}	
+		}
+		//异常测试
+		@Test
+		public void test4() {
+			System.out.println(1/0);
+		}
 }
